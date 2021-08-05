5c4def417b24c2314602a67dde46b8b0c8bb3735
Natalie-Cardot <nataliecardot@gmail.com>
2021-08-04 20:20:51 -0700

    Dynamically set number of submenu columns

--- src/Submenu.js
+++ src/Submenu.js
@@ -8,12 +8,19 @@ const Submenu = () => {
     location,
   } = useGlobalContext();
   const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
+
   useEffect(() => {
+    // Default state value to prevent bugs
+    setColumns('col-2');
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  }, [location]);
+
+    if (links.length === 3) setColumns('col-3');
+    if (links.length > 3) setColumns('col-4');
+  }, [location, links]);
 
   return (
     <aside
@@ -21,7 +28,7 @@ const Submenu = () => {
       ref={container}
     >
       <h4>{page}</h4>
-      <div className={`submenu-center col-2`}>
+      <div className={`submenu-center ${columns}`}>
         {links.map((link, index) => {
           const { label, icon, url } = link;
           return (
